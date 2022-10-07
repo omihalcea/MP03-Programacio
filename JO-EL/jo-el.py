66f2db0e7e265fffd855a3ad260a33d7436abd2f
omihalcea <omihalcea@iesebre.com>
2022-10-07 14:22:48 +0200

    joel

--- JO-EL/jo-el.py
+++ JO-EL/jo-el.py
@@ -1,2 +1,13 @@
-capa = int(input())
+n = int(input())
+deus = int()
+nota = int()
 
+while n != int():
+    n = int(input())
+    if n == 10:
+        deus += 1
+        nota += 1
+    elif 0 <= n <= 10:
+        nota += 1
+
+print("TOTAL NOTES: " + str(nota) + " NOTES10: " + str(deus))
