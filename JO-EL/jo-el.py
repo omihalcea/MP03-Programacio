9a69de1dba542174a167b2ee53cee0b55aa8c58c
omihalcea <omihalcea@iesebre.com>
2022-10-10 14:27:25 +0200

    fgfghg

--- JO-EL/jo-el.py
+++ JO-EL/jo-el.py
@@ -1,13 +1,11 @@
-n = int(input())
-deus = int()
-nota = int()
+p1 = int(input())
+p2 = int(input())
 
-while n != int():
-    n = int(input())
-    if n == 10:
-        deus += 1
-        nota += 1
-    elif 0 <= n <= 10:
-        nota += 1
-
-print("TOTAL NOTES: " + str(nota) + " NOTES10: " + str(deus))
+if p1 == 1 and p2 == 3 or p1 == 2 and p2 == 1 or p1 == 3 and p2 == 2:
+    print("Jugador1")
+elif p2 == 1 and p1 == 3 or p2 == 2 and p1 == 1 or p2 == 3 and p1 == 2:
+    print("Jugador2")
+elif p1 == p2:
+    print("EMPAT")
+elif p1 != 1:
+    print("ERROR")
