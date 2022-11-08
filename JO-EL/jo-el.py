23fb145b7fee350b71ddfd18afa1d11aa5d1c9d9
omihalcea <omihalcea@iesebre.com>
2022-11-08 14:23:23 +0100

    casiacabat

--- JO-EL/jo-el.py
+++ JO-EL/jo-el.py
@@ -1,6 +1,16 @@
-i = input().split(";")
-
-for x in i:
-    if len(x) >= 10:
-        i[i.index(x)] = "ERROR"
-print(";".join(i))
+i=int(input())
+while i-1>0:
+    n=list(map(int, input().split()))
+    l=[]
+    while (n[0]-1)>0:
+        fil=l.append(list(map(int, input().split())))
+        n[0]-=1
+    posicio=list(map(int, input().split()))
+    uns=int()
+    if l[posicio[0]][posicio[1]]==1:
+        print("BOOM")
+    else:
+        for u in l:
+            uns+=u.count(1)
+        print(uns)
+    i-=1
--- UF1/Projecte/projecte.py
+++ UF1/Projecte/projecte.py
@@ -1,15 +1,21 @@
 import random
+nombre = 1
 t1 = random.randint(1, 2)
 t2 = int()
 np = 3
 rang = 2
 puntuacio1 = int()
 puntuacio2 = int()
-preguntes = [["Quants anys tinc? \n[1] 1 \n[2] 2 \n[3] 3", 2], 
-            ["T'agrada el Pene? \n[1] SI \n[2] Potser \n[3] No estic del tot segur", 3], 
-            ["Ets menor \n[1] SI \n[2] NO", 2]]
+puntuacio3 = int()
+puntuacio4 = int()
+jugadors = []
+preguntes = [
+    ["Quants anys tinc? \n[1] 1 \n[2] 2 \n[3] 3", 2], 
+    ["T'agrada el Pene? \n[1] SI \n[2] Potser \n[3] No estic del tot segur", 3], 
+    ["Ets menor \n[1] SI \n[2] NO" "\n[3] NO SE", 2]
+    ]
 
-nomp1 = input("""\n─────¡¡¡Benvinguts al Trivi Pursuit!!!─────
+numjugadors = int(input("""\n─────¡¡¡Benvinguts al Trivia Pursuit!!!─────
 
 Este es un joc de preguntes per a 2 jugadors,
 el primer que encerte 3 preguntes guanya.
@@ -19,38 +25,40 @@ qu hi ha al costat de cada resposta.
 
 Per a continuar heu d'introduir els vostres noms
 
-Nom del jugador 1: """)
-nomp2 = input("Ara introduiu el nom del segon jugador: ")
+Introdueix el numero de jugadors (2-4): """))
 
-if t1 == 1:
-    print("\nComença el jugador 1")
-    t2 = 2
-else:
-    print("\nComença el jugador 2")
-    t2 = 1
+los = numjugadors
 
-while puntuacio1 < 3 or puntuacio2 < 3:
+while numjugadors > 0:
+    nomp = input("Introduiu el nom del jugador " + str(nombre) + ": ")
+    jugadors.append(nomp)
+    nombre += 1
+    numjugadors -=1
+
+while puntuacio1 < 3 and puntuacio2 < 3 and puntuacio3 < 3 and puntuacio4 < 3 and len(preguntes) != 0:
+    turno = random.randint(0, los-1)
+    print("\nEs el torn de", str(jugadors[turno]))
     while np > 0:
-        pregunta = random.randint(0, rang)
-        print("\n" + preguntes[pregunta][0] + "\n")
+        random.shuffle(preguntes)
+        print("\n" + preguntes[0][0] + "\n")
         res = int(input())
-        preguntes.pop(pregunta)
         rang -= 1
-        if t1 == 1 and res == preguntes[pregunta][1]:
+        if res == preguntes[0][1]:
             print("\nResposta correcta")
-            puntuacio1 += 1
-
-        elif t2 == 1 and res == preguntes[pregunta][1]:
-            print("\nResposta correcta")
-            puntuacio2 += 2
-
-        elif t1 == 1 and res != preguntes[pregunta][1]:
-            print("\nResposta incorrecta")
-            t1 = 2
-            t2 = 1
-            
-        elif t2 == 1 and res != preguntes[pregunta][1]:
-            print("\nResposta incorrecta")
-            t1 = 1
-            t2 = 2
+            del preguntes[0]
+            if turno == 1:
+                puntuacio1 += 1
+            elif turno == 2:
+                puntuacio2 += 1
+            elif turno == 3:
+                puntuacio3 += 1
+            elif turno == 4:
+                puntuacio4 += 1
+        else:
+            print("\nRESPOSTA INCORRECTA\n")
+            del preguntes[0]
+            turno += 1
+            if turno + 1 > los:
+                turno = 0
+            print("\nEs el torn de", str(jugadors[turno]))
         np -= 1
