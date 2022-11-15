a0891810de220221f1c0c692fb9731f9c179269d
omihalcea <omihalcea@iesebre.com>
2022-11-15 14:22:32 +0100

    acabat

--- UF1/Projecte/projecte.py
+++ UF1/Projecte/projecte.py
@@ -1,33 +1,39 @@
 import random
+from os import system
+import time
 nombre = 1
-t1 = random.randint(1, 2)
-t2 = int()
-np = 3
-rang = 2
 puntuacio1 = int()
 puntuacio2 = int()
 puntuacio3 = int()
 puntuacio4 = int()
+puntuacions = [puntuacio1, puntuacio2, puntuacio3, puntuacio4]
 jugadors = []
 preguntes = [
     ["Qui va ser el primer president d'Estats Units?",[["Abraham Lincon", False], ["George Washinton", True], ["Donald Trump", False]]], 
-    ["T'agrada el Pene? \n[1] SI \n[2] Potser \n[3] No estic del tot segur",[["Abraham Lincon", False], ["George Washinton", True], ["Donald Trump", False]]], 
-    ["Ets menor \n[1] SI \n[2] NO" "\n[3] NO SE",[["Abraham Lincon", False], ["George Washinton", True], ["Donald Trump", False]]]
+    ["Quin any va arribar l'home a la lluna?",[["1969", True], ["1970", False], ["1965", False]]], 
+    ["En quin any es va disoldre l'unio sovietica?",[["1987", False], ["1991", True], ["1976", False]]],
+    ["Quina es la capital de Grecia?",[["Mikonos", False], ["Creta", False], ["Atenes", True]]],
+    ["En quins oceans fa contacte Japó?",[["Pacific i Índic", False], ["Atlantic i Nordic", False], ["Pacific", True]]],
+    ["Quina d'aquestes tribus no es nativa Americana?",[["Cheroquis", False], ["Nunga", True], ["Apaches", False]]],
+    ["Com es diu la linea imaginaria vertical que divideix el planeta per la meitat?",[["El meridia de Greenwitch", True], ["Ecuador", False], ["Tròpic de Càncer", False]]],
+    ["Quin dels següents programes no es un SGBD?",[["Microsoft Word", True], ["Oracle", False], ["MySQL", False]]],
+    ["Qui va pintar el Guernica?",[["Salvador Dalí", False], ["Jackson Pollock", False], ["Pablo Picasso", True]]],
+    ["Tinc una raqueta i un pilota de tenis. Tot junt m'ha costat 1.10€ i se que la raqueta m'ha costat 1€ mes que la pilota. Quan costa la pilota?",[["0.10€", False], ["0.5€", False], ["0.05€", True]]],
     ]
 
 numjugadors = int(input("""\n─────¡¡¡Benvinguts al Trivia Pursuit!!!─────
 
-Este es un joc de preguntes per a 2 jugadors,
+Este es un joc de preguntes de 2 a 4 jugadors,
 el primer que encerte 3 preguntes guanya.
 
 Per a contestar has d'escriure el numero
-qu hi ha al costat de cada resposta.
+que hi ha al costat de cada resposta.
 
-Per a continuar heu d'introduir els vostres noms
+Per a continuar heu d'introduir quants jugadors sereu.
 
 Introdueix el numero de jugadors (2-4): """))
 
-los = numjugadors
+los = numjugadors-1
 numr = [1, 2, 3]
 while numjugadors > 0:
     nomp = input("Introduiu el nom del jugador " + str(nombre) + ": ")
@@ -35,34 +41,42 @@ while numjugadors > 0:
     nombre += 1
     numjugadors -=1
 
-while puntuacio1 < 3 and puntuacio2 < 3 and puntuacio3 < 3 and puntuacio4 < 3 and len(preguntes) != 0:
-    turno = random.randint(0, los-1)
-    print("\nEs el torn de", str(jugadors[turno]))
-    while np > 0:
-        random.shuffle(preguntes)
-        random.shuffle(preguntes[0][1])
-        print("\n" + preguntes[0][0] + "\n")
-        for i in range(1, 4):
-            print("[" + str(i) + "]",preguntes[0][1][i-1][0])
+turno = random.randint(0, los)
 
-        res = int(input())
-        rang -= 1
-        if res == preguntes[0][1]:
+while puntuacio1 < 3 and puntuacio2 < 3 and puntuacio3 < 3 and puntuacio4 < 3 and len(preguntes) > 0:
+    random.shuffle(preguntes)
+    random.shuffle(preguntes[0][1])
+    print("\nEs el torn de", str(jugadors[turno]))
+    print("\n" + preguntes[0][0] + "\n")
+    for i in range(1, 4):
+        print("[" + str(i) + "]",preguntes[0][1][i-1][0])  
+    res = int(input())
+    if preguntes[0][1][res-1][1] == True:
+        if len(preguntes) > 0:
             print("\nResposta correcta")
             del preguntes[0]
-            if turno == 1:
+            if turno+1 == 1:
                 puntuacio1 += 1
-            elif turno == 2:
+            elif turno+1 == 2:
                 puntuacio2 += 1
-            elif turno == 3:
+            elif turno+1 == 3:
                 puntuacio3 += 1
-            elif turno == 4:
+            elif turno+1 == 4:
                 puntuacio4 += 1
-        else:
+            input("\nPrem enter per continuar")
+            system("clear")
+    else:
+        if len(preguntes) > 0:
             print("\nRESPOSTA INCORRECTA\n")
             del preguntes[0]
-            turno += 1
             if turno + 1 > los:
                 turno = 0
-            print("\nEs el torn de", str(jugadors[turno]))
-        np -= 1
+            else:
+                turno += 1
+        input("\nPrem enter per continuar")
+        system("clear")
+
+if puntuacio1 == 3 or puntuacio2 == 3 or puntuacio3 == 3 or puntuacio4 == 3:
+    print("El guanyador es", jugadors[turno] + ", felicitats!!!")
+else:
+    print("S'han acabat les preguntes i no ha guanyat ningú.")
